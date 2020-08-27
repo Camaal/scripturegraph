ea61c8972b439b2ab861106ae0a3224715290544
Camaal Moten <cmoten@fullsail.com>
2020-08-26 20:57:27 -0400

    Added a new table for target verses

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-import sqlalchemy as db
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import func
-from database import Books, References, Scripture, Base
+from database import Sources, Base
 
 
-engine = create_engine('sqlite:///bible.db')
+engine: object = create_engine('sqlite:///bible.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
@@ -20,30 +18,28 @@ session = DBSession()
 @app.route('/list')
 def showBooks():
     #List all books in order they were written
-    books = session.query(Scripture.book_name).distinct().order_by(Scripture.book)
+    books = session.query(Sources.book_name).distinct().order_by(Sources.book)
 
     #List all the chapters in order
-    chapters = session.query(Scripture.chapter).distinct().order_by(Scripture.chapter)
+    chapters = session.query(Sources.chapter).distinct().order_by(Sources.chapter)
 
     #List all the authors of each book and chapter
-    authors = session.query(Scripture.Author).distinct().order_by(Scripture.Author)
+    authors = session.query(Sources.Author).distinct().order_by(Sources.Author)
 
     #Calculate the average degree of a given book (eventually replace the book with a variable triggered by a button)
-    avgdegree = session.query(func.avg(Scripture.Degree)).filter_by(book=1).all()
+    avgdegree = session.query(func.avg(Sources.Degree)).filter_by(book=1).all()
 
     #Calculate min degree
-    mindegree = session.query(func.min(Scripture.Degree)).filter_by(book=1).all()
+    mindegree = session.query(func.min(Sources.Degree)).filter_by(book=1).all()
 
     #Calculate max degree
-    maxdegree = session.query(func.max(Scripture.Degree)).filter_by(book=1).all()
+    maxdegree = session.query(func.max(Sources.Degree)).filter_by(book=1).all()
 
     #List distinct chapters for a given book
-    dchapters = session.query(Scripture.chapter).distinct().filter_by(book=1).count()
+    dchapters = session.query(Sources.chapter).distinct().filter_by(book=1).count()
 
     #List chapters, verses, and text for a given book
-    dverses = session.query(Scripture.chapter, Scripture.verse, Scripture.text, Scripture.Degree).filter_by(book=1)
-
-    #references = session.query(References).join(Scripture).filter(Scripture.book==1).all()
+    dverses = session.query(Sources.chapter, Sources.verse, Sources.text, Sources.Degree).filter_by(book=1)
 
     #Return the data to list.html
     return render_template('list.html',
@@ -54,8 +50,7 @@ def showBooks():
                            dverses=dverses,
                            avgdegree=avgdegree,
                            mindegree=mindegree,
-                           maxdegree=maxdegree,
-                           #references=references
+                           maxdegree=maxdegree
                            )
 
 
--- database.py
+++ database.py
@@ -16,19 +16,16 @@ class Books(Base):
     genre_name = Column(String(250), nullable=False)
     book = Column(Integer, primary_key=True)
 
-# Cross-references between scripture
 class References(Base):
     __tablename__ = "references"
 
     Id = Column(Integer, primary_key=True)
-    Source = Column(Integer, ForeignKey('scripture.Id'))
-    Target = Column(Integer, ForeignKey('scripture.Id'))
+    Source = Column(Integer, ForeignKey('source.Id'))
+    Target = Column(Integer, ForeignKey('target.Id'))
 
-    scriptures = relationship('scripture', back_populates="references")
-
-# The actual scripture
-class Scripture(Base):
-    __tablename__ = "scripture"
+# The source scripture
+class Sources(Base):
+    __tablename__ = "source"
 
     Id = Column(Integer, primary_key=True)
     red_letter = Column(Boolean, nullable=False)
@@ -41,6 +38,27 @@ class Scripture(Base):
     verse = Column(Integer, nullable=False)
     text = Column(Text, nullable=False)
     Degree = Column(Integer, nullable=False)
+
+    targets = relationship('References', back_populates="sources")
+
+# The source scripture
+class Targets(Base):
+    __tablename__ = "target"
+
+    Id = Column(Integer, primary_key=True)
+    red_letter = Column(Boolean, nullable=False)
+    Author = Column(String(250), nullable=False)
+    book_name = Column(String(250), nullable=False)
+    testament = Column(String(2), nullable=False)
+    genre_name = Column(String(250), nullable=False)
+    book = Column(Integer, nullable=False)
+    chapter = Column(Integer, nullable=False)
+    verse = Column(Integer, nullable=False)
+    text = Column(Text, nullable=False)
+    Degree = Column(Integer, nullable=False)
+
+    sources = relationship('References', back_populates="targets")
+
 
 # Deprecated table that housed all the data (too slow)
 # class Network(Base):
