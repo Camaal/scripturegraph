98cf3705ff4203dee4f67d8b92b6e574f8b6daed
Camaal Moten <cmoten@fullsail.com>
2020-08-31 19:15:19 -0400

    I removed the extra network diagram file. I also found a basic way of passing variable to change the book and cross-referenced verses based on selections in the side nav and main text area.

--- app/routes.py
+++ app/routes.py
@@ -22,8 +22,8 @@ def flat_list(l):
     return ["%s" % v for v in l]
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/')
+@app.route('/index')
 def index():
 
     # List all books in order they were written
@@ -33,7 +33,7 @@ def index():
         .order_by(Authors.book).all()
 
     # Sum degree for each book
-    bookDegrees = db.session.query(Sources.book_name, func.sum(Sources.degree).label('total')).group_by(
+    bookDegrees = db.session.query(Sources.book, Sources.book_name, func.sum(Sources.degree).label('total')).group_by(
         Sources.book_name).order_by(Sources.book).all()
 
     # Sum degree for each chapter
@@ -58,7 +58,7 @@ def index():
 
     # List book, book name, chapter, verse, text and degree for cross-references related to Gen 1:1
     joins = db.session.query(Targets.book_name, Targets.book, Targets.chapter, Targets.verse, Targets.text,
-                             Targets.degree, Targets.color, Targets.norm_degree).join(References)\
+                             Targets.degree, Targets.color, Targets.norm_degree).join(References) \
         .filter(References.Source == 1001001).all()
 
     # List authors related to Gen 1:1
@@ -71,12 +71,69 @@ def index():
                            title='Home',
                            books=books,
                            authors=authors,
-                           dchapters=dchapters,
-                           dverses=dverses,
-                           joins=joins,
-                           tbooks=tbooks,
                            bookDegrees=bookDegrees,
                            chapterDegrees=chapterDegrees,
                            authorDegrees=authorDegrees,
+                           dchapters=dchapters,
+                           dverses=dverses,
+                           tbooks=tbooks,
+                           joins=joins,
+                           tauthors=tauthors
+                           )
+
+@app.route('/book/<int:b>/verse/<int:v>')
+def index_book(b, v):
+    # List all books in order they were written
+    books = db.session.query(Books.book_name).distinct().order_by(Books.book)
+
+    authors = db.session.query(Authors.author, Authors.book_name, Authors.book, Authors.chapter) \
+        .order_by(Authors.book).all()
+
+    # Sum degree for each book
+    bookDegrees = db.session.query(Sources.book, Sources.book_name, func.sum(Sources.degree).label('total')).group_by(
+        Sources.book_name).order_by(Sources.book).all()
+
+    # Sum degree for each chapter
+    chapterDegrees = db.session.query(Sources.chapter, func.sum(Sources.degree).label('total')).group_by(
+        Sources.chapter).order_by(Sources.chapter).all()
+
+    # Sum degree for each chapter
+    authorDegrees = db.session.query(Sources.author, func.sum(Sources.degree).label('total')).group_by(
+        Sources.author).order_by(Sources.author).all()
+
+    # List distinct chapters for a given book
+    dchapters = db.session.query(Sources.chapter).distinct().filter_by(book=b).count()
+
+    # List chapters, verses, and text for a given book
+    dverses = db.session.query(Sources.Id, Sources.book, Sources.chapter, Sources.verse, Sources.text, Sources.degree,
+                               Sources.color, Sources.norm_degree).filter_by(book=b)
+
+    # List book, book name, and chapter for cross-references related to Gen 1:1
+    tbooks = db.session.query(Targets.book, Targets.book_name, Targets.chapter, Targets.author).distinct().join(
+        References).filter(
+        References.Source == v).order_by(Targets.book).distinct()
+
+    # List book, book name, chapter, verse, text and degree for cross-references related to Gen 1:1
+    joins = db.session.query(Targets.book_name, Targets.book, Targets.chapter, Targets.verse, Targets.text,
+                             Targets.degree, Targets.color, Targets.norm_degree).join(References) \
+        .filter(References.Source == v).all()
+
+    # List authors related to Gen 1:1
+    tauthors = db.session.query(Targets.author).join(References) \
+        .filter(References.Source == v).distinct()
+
+    # Return the data to index.html
+
+    return render_template('index.html',
+                           title='Home',
+                           books=books,
+                           authors=authors,
+                           bookDegrees=bookDegrees,
+                           chapterDegrees=chapterDegrees,
+                           authorDegrees=authorDegrees,
+                           dchapters=dchapters,
+                           dverses=dverses,
+                           tbooks=tbooks,
+                           joins=joins,
                            tauthors=tauthors
                            )
