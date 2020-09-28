9f65c7941362e2d73a0882a586ad33f3ea659972
Camaal Moten <cmoten@fullsail.com>
2020-09-28 04:22:26 -0400

    Package updates and attempting to load a desktop app.

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,23 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flaskwebgui import FlaskUI #get the FlaskUI class
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
 from app import routes, models
+
+# Feed it the flask app instance
+ui = FlaskUI(app, host="127.0.0.1", port="8000", width=1920, height=1080)
+
+# call the 'run' method
+ui.run()
+
+
+
+
+
