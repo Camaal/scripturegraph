d073e1888e7c246a500b638aa086e45cac1286c7
Camaal Moten <cmoten@fullsail.com>
2020-11-28 06:55:04 -0500

    Code cleanup

--- config.py
+++ config.py
@@ -10,21 +10,3 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'c3b1ddc124344a8b8bda21e13b722369'
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/biblegraph'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-class ProductionConfig(Config):
-    DEBUG = False
-
-
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class DevelopmentConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class TestingConfig(Config):
-    TESTING = True
