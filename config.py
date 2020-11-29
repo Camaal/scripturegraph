d551f2982da5e11a38f2a0d5034006f95525eba6
Camaal Moten <cmoten@fullsail.com>
2020-11-29 10:39:20 -0500

    Adding psycopg2 to requirements

--- config.py
+++ config.py
@@ -7,5 +7,6 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/biblegraph'
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL' or 'postgresql://postgres:postgres@localhost:5432/biblegraph')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
