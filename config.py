49d40d1407c214919a3288b4d70e86dd37e98728
Camaal Moten <cmoten@fullsail.com>
2020-11-29 11:46:58 -0500

    More config adjustments to align with Heroku

--- config.py
+++ config.py
@@ -4,9 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    DEBUG = False
-    TESTING = False
-    CSRF_ENABLED = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL' or 'postgresql://postgres:postgres@localhost:5432/biblegraph')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
